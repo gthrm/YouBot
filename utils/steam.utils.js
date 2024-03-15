29b5a4e963bd78cd5ffe2f8eae539015aeb3cd05
cdROma <38502733+gthrm@users.noreply.github.com>
2024-03-15 11:32:15 +0300

    fix

--- utils/steam.utils.js
+++ utils/steam.utils.js
@@ -1,6 +1,6 @@
 const readline = require('readline');
 const aws = require('aws-sdk');
-const fs = require('fs');
+const fs = require('fs').promises;
 const ytdl = require('ytdl-core');
 const ffmpeg = require('fluent-ffmpeg');
 const { v4: uuidv4 } = require('uuid');
@@ -25,44 +25,54 @@ function calculateDuration(lengthSeconds) {
   return Number(MAX_DURATION);
 }
 
-const saveVideoAsMP3 = async (url, userId, next = () => { }) => {
+const saveVideoAsMP3 = async (url, userId, next = () => {}) => {
   const videoId = uuidv4();
-  const info = await ytdl.getInfo(url);
-
-  const stream = ytdl(url, {
-    quality: 'highestaudio',
-  });
   const fileName = `${__dirname}/${videoId}.mp3`;
   const key = `${userId}/${videoId}.mp3`;
   const start = Date.now();
-  const duration = calculateDuration(info?.videoDetails.lengthSeconds);
+  try {
+    const info = await ytdl.getInfo(url);
+    const stream = ytdl(url, {
+      quality: 'highestaudio',
+    });
+    const duration = calculateDuration(info?.videoDetails.lengthSeconds);
+    logger.info(`Duration has been calculated: ${duration} sec.`, duration);
 
-  logger.info(`Duration has been calculated: ${duration} sec.`, duration);
-  return ffmpeg(stream)
-    .audioBitrate(128)
-    .setDuration(duration)
-    .save(fileName)
-    .on('progress', (p) => {
-      readline.cursorTo(process.stdout, 0);
-      const dowloadMessage = `${p.targetSize}kb downloaded; timemark: ${p.timemark}; currentKbps: ${p.currentKbps}`;
-      process.stdout.write(dowloadMessage);
-    })
-    .on('end', async () => {
-      const fileContent = await fs.readFileSync(fileName);
-      const params = {
-        Bucket: SPACE_NAME,
-        Key: key,
-        Body: fileContent,
-        ACL: 'public-read',
-      };
-      logger.info('\nUpload to space started');
-      const data = await s3.putObject(params).promise();
-      await fs.unlinkSync(fileName);
-      logger.info('\nDownload complete', data);
-      logger.info(`\ndone, thanks - ${(Date.now() - start) / 1000}s`);
-      next({ fileName, key, info });
-    })
-    .on('error', (err) => logger.error(err));
+    await new Promise((resolve, reject) => {
+      ffmpeg(stream)
+        .audioBitrate(128)
+        .setDuration(duration)
+        .save(fileName)
+        .on('progress', (p) => {
+          readline.cursorTo(process.stdout, 0);
+          const dowloadMessage = `${p.targetSize}kb downloaded; timemark: ${p.timemark}; currentKbps: ${p.currentKbps}`;
+          process.stdout.write(dowloadMessage);
+        })
+        .on('end', () => resolve())
+        .on('error', (err) => reject(err));
+    });
+
+    const fileContent = await fs.readFile(fileName);
+    const params = {
+      Bucket: SPACE_NAME,
+      Key: key,
+      Body: fileContent,
+      ACL: 'public-read',
+    };
+    logger.info('\nUpload to space started');
+    const data = await s3.putObject(params).promise();
+    await fs.unlink(fileName);
+    logger.info('\nDownload complete', data);
+    logger.info(`\ndone, thanks - ${(Date.now() - start) / 1000}s`);
+    next({ fileName, key, info });
+  } catch (err) {
+    logger.error(err);
+    try {
+      await fs.unlink(fileName);
+    } catch (e) {
+      logger.error('Error removing file', e);
+    }
+  }
 };
 
 module.exports = {
